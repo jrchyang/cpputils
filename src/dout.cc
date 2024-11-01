2847245b4a5417a52abd9eb1537f13143af5ff9b
jrchyang <yujrchyang@gmail.com>
2024-11-01 09:39:16 +0000

    just modify format

--- src/dout.cc
+++ src/dout.cc
@@ -3,14 +3,16 @@
 
 namespace TOPNSPC {
 
-void dout_emergency(const char * const str) {
-  std::cerr << str;
-  std::cerr.flush();
+void dout_emergency(const char * const str)
+{
+	std::cerr << str;
+	std::cerr.flush();
 }
 
-void dout_emergency(const std::string &str) {
-  std::cerr << str;
-  std::cerr.flush();
+void dout_emergency(const std::string &str)
+{
+	std::cerr << str;
+	std::cerr.flush();
 }
 
 }
